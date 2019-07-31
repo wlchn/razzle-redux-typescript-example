d932a15fd9b6574380c5b211042b976b8ebaf4f4
wlchn <wlchn@qq.com>
2019-07-31 17:26:02 +0800

    check path

--- src/server.ts
+++ src/server.ts
@@ -27,7 +27,7 @@ server
   .disable('x-powered-by')
   .use(express.static(process.env.RAZZLE_PUBLIC_DIR || ''))
   .get('/*', (req, res) => {
-    if (req.path.includes('%') || req.url.includes('script')) {
+    if (req.path.includes('%') || req.url.toLowerCase().includes('script')) {
       res.redirect('/');
     }
     res.append('X-XSS-Protection', '1; mode=block');
